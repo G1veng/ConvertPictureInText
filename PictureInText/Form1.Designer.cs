b8fdbac70646e153794e6ef35f66ab6020b1bf21
G1veng <daniil-shishko@mail.ru>
2021-07-03 12:31:35 +0300

    Add project files.

---
+++ PictureInText/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace PictureInText
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+      this.button1 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox1.Location = new System.Drawing.Point(12, 56);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(590, 382);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox1.TabIndex = 4;
+      this.pictureBox1.TabStop = false;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox1.Location = new System.Drawing.Point(608, 56);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(180, 382);
+      this.textBox1.TabIndex = 8;
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.button2.Location = new System.Drawing.Point(649, 5);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(102, 45);
+      this.button2.TabIndex = 9;
+      this.button2.Text = "Save results";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+      this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.button3.Location = new System.Drawing.Point(423, 5);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(102, 45);
+      this.button3.TabIndex = 10;
+      this.button3.Text = "Analize";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // checkedListBox1
+      // 
+      this.checkedListBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+      this.checkedListBox1.CheckOnClick = true;
+      this.checkedListBox1.FormattingEnabled = true;
+      this.checkedListBox1.Items.AddRange(new object[] {
+            "IronOcr",
+            "AspriseOCR",
+            "Vintasoft"});
+      this.checkedListBox1.Location = new System.Drawing.Point(247, 1);
+      this.checkedListBox1.MultiColumn = true;
+      this.checkedListBox1.Name = "checkedListBox1";
+      this.checkedListBox1.Size = new System.Drawing.Size(81, 49);
+      this.checkedListBox1.TabIndex = 11;
+      this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+      // 
+      // button1
+      // 
+      this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.button1.Location = new System.Drawing.Point(74, 5);
+      this.button1.MaximumSize = new System.Drawing.Size(102, 100);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(102, 45);
+      this.button1.TabIndex = 12;
+      this.button1.Text = "Set picture";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.checkedListBox1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.pictureBox1);
+      this.Name = "Form1";
+      this.Text = "PictureInText";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.CheckedListBox checkedListBox1;
+    private System.Windows.Forms.Button button1;
+  }
+}
+
---
+++ PictureInText/Form1.cs
@@ -0,0 +1,527 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using IronOcr;
+using asprise_ocr_api;
+using System.IO;
+using Vintasoft;
+
+namespace PictureInText
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    static Bitmap ScaleImage(Image source, int width, int height)
+    {
+
+      Bitmap dest = new Bitmap(width, height);
+      using (Graphics gr = Graphics.FromImage(dest))
+      {
+        gr.FillRectangle(Brushes.White, 0, 0, width, height);
+        gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+        float srcwidth = source.Width;
+        float srcheight = source.Height;
+        float dstwidth = width;
+        float dstheight = height;
+
+        if (srcwidth <= dstwidth && srcheight <= dstheight)
+        {
+          int left = (width - source.Width) / 2;
+          int top = (height - source.Height) / 2;
+          gr.DrawImage(source, left, top, source.Width, source.Height);
+        }
+        else if (srcwidth / srcheight > dstwidth / dstheight)
+        {
+          float cy = srcheight / srcwidth * dstwidth;
+          float top = ((float)dstheight - cy) / 2.0f;
+          if (top < 1.0f) top = 0;
+          gr.DrawImage(source, 0, top, dstwidth, cy);
+        }
+        else
+        {
+          float cx = srcwidth / srcheight * dstheight;
+          float left = ((float)dstwidth - cx) / 2.0f;
+          if (left < 1.0f) left = 0;
+          gr.DrawImage(source, left, 0, cx, dstheight);
+        }
+
+        return dest;
+      }
+    }
+
+    static void ConverPicture(Bitmap newBitmap)
+    {
+      StreamWriter file = new StreamWriter("Picture.txt", true);
+      var backGroundColor = (1.0 - (newBitmap.GetPixel(0, 0).R / 255));
+      if (backGroundColor == 0)
+      {
+        for (int i = 0; i < newBitmap.Height; i++)
+        {
+          for (int j = 0; j < newBitmap.Width; j++)
+          {
+            var gray = (1.0 - (newBitmap.GetPixel(j, i).R / 255));
+            file.Write(gray);
+            file.Write(" ");
+          }
+        }
+        file.Close();
+      }
+      if (backGroundColor == 1)
+      {
+        for (int i = 0; i < newBitmap.Height; i++)
+        {
+          for (int j = 0; j < newBitmap.Width; j++)
+          {
+            var gray = (1.0 - (newBitmap.GetPixel(j, i).R / 255));
+            if (gray == 1)
+              gray = 0;
+            else
+              gray = 1;
+            file.Write(gray);
+            file.Write(" ");
+          }
+        }
+        file.Close();
+      }
+    }
+
+    public class NeuralNetwork
+    {
+
+      int[] layer; //layer information
+      Layer[] layers; //layers in the network
+
+
+      public float getAverageError()
+      {
+        return layers[layers.Length - 1].getAverageError();
+      }
+
+      public void saveWeights()
+      {
+
+        /*for (int i = layers.Length - 1; i > 0; i--)
+        {
+          layers[i].saveWeights(layer[i], layer[i + 1]);
+        }*/
+        for (int i = 0; i < layers.Length; i++)
+        {
+          layers[i].saveWeights(layer[i], layer[i + 1]);
+        }
+      }
+      public NeuralNetwork(int[] layer)
+      {
+        //deep copy layers
+        this.layer = new int[layer.Length];
+        for (int i = 0; i < layer.Length; i++)
+          this.layer[i] = layer[i];
+
+        //creates neural layers
+        layers = new Layer[layer.Length - 1];
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+          layers[i] = new Layer(layer[i], layer[i + 1]);
+        }
+      }
+
+      public float[] FeedForward(float[] inputs)
+      {
+        //feed forward
+        layers[0].FeedForward(inputs);
+        for (int i = 1; i < layers.Length; i++)
+        {
+          layers[i].FeedForward(layers[i - 1].outputs);
+        }
+
+        return layers[layers.Length - 1].outputs; //return output of last layer
+      }
+
+      public void BackProp(float[] expected)
+      {
+        // run over all layers backwards
+        for (int i = layers.Length - 1; i >= 0; i--)
+        {
+          if (i == layers.Length - 1)
+          {
+            layers[i].BackPropOutput(expected); //back prop output
+          }
+          else
+          {
+            layers[i].BackPropHidden(layers[i + 1].gamma, layers[i + 1].weights); //back prop hidden
+          }
+        }
+
+        //Update weights
+        for (int i = 0; i < layers.Length; i++)
+        {
+          layers[i].UpdateWeights();
+        }
+      }
+
+      public class Layer
+      {
+        int numberOfInputs; //# of neurons in the previous layer
+        int numberOfOuputs; //# of neurons in the current layer
+
+
+        public float[] outputs; //outputs of this layer
+        public float[] inputs; //inputs in into this layer
+        public float[,] weights; //weights of this layer
+        public float[,] weightsDelta; //deltas of this layer
+        public float[] gamma; //gamma of this layer
+        public float[] error; //error of the output layer
+        public float averageError;
+
+        public static Random random = new Random(); //Static random class variable
+
+
+        public void saveWeights(int numberOfInputs, int numberOfOuputs)
+        {
+          StreamWriter someWeights = new StreamWriter("Weights.txt", true);
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            for (int j = 0; j < numberOfInputs; j++)
+            {
+              someWeights.WriteLine(weights[i, j]);
+            }
+          }
+          someWeights.Close();
+        }
+        public float getAverageError()
+        {
+          return averageError;
+        }
+        public Layer(int numberOfInputs, int numberOfOuputs)
+        {
+          this.numberOfInputs = numberOfInputs;
+          this.numberOfOuputs = numberOfOuputs;
+
+          //initilize datastructures
+          outputs = new float[numberOfOuputs];
+          inputs = new float[numberOfInputs];
+          weights = new float[numberOfOuputs, numberOfInputs];
+          weightsDelta = new float[numberOfOuputs, numberOfInputs];
+          gamma = new float[numberOfOuputs];
+          error = new float[numberOfOuputs];
+          if (numberOfInputs == 1024)
+          {
+            InitilizeWeights0(false); //initilize weights
+          }
+          else
+          {
+            InitilizeWeights1(false);
+          }
+        }
+
+        public void InitilizeWeights0(bool randomWeigths)
+        {
+          if (randomWeigths)
+          {
+            for (int i = 0; i < numberOfOuputs; i++)
+            {
+              for (int j = 0; j < numberOfInputs; j++)
+              {
+                weights[i, j] = (float)random.NextDouble() - 0.5f;
+              }
+            }
+          }
+          else
+          {
+            StreamReader someWeights = new StreamReader("Weights.txt", true);
+            for (int i = 0; i < numberOfOuputs; i++)
+            {
+              for (int j = 0; j < numberOfInputs; j++)
+              {
+                weights[i, j] = float.Parse(someWeights.ReadLine());
+              }
+            }
+            someWeights.Close();
+          }
+        }
+
+        public void InitilizeWeights1(bool randomWeigths)
+        {
+          if (randomWeigths)
+          {
+            for (int i = 0; i < numberOfOuputs; i++)
+            {
+              for (int j = 0; j < numberOfInputs; j++)
+              {
+                weights[i, j] = (float)random.NextDouble() - 0.5f;
+              }
+            }
+          }
+          else
+          {
+            StreamReader someWeights = new StreamReader("Weights.txt", true);
+            for (int i = 0; i < 1024; i++)
+            {
+              for (int j = 0; j < 500; j++)
+              {
+                //weights[i, j] = float.Parse(someWeights.ReadLine());
+                someWeights.ReadLine();
+              }
+            }
+            for (int i = 0; i < numberOfOuputs; i++)
+            {
+              for (int j = 0; j < numberOfInputs; j++)
+              {
+                weights[i, j] = float.Parse(someWeights.ReadLine());
+              }
+            }
+            someWeights.Close();
+          }
+        }
+
+        public float[] FeedForward(float[] inputs)
+        {
+          this.inputs = inputs;// keep shallow copy which can be used for back propagation
+
+          //feed forwards
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            outputs[i] = 0;
+            for (int j = 0; j < numberOfInputs; j++)
+            {
+              outputs[i] += inputs[j] * weights[i, j];
+            }
+
+            outputs[i] = (float)Math.Tanh(outputs[i]);
+          }
+
+          return outputs;
+        }
+
+        public float TanHDer(float value)
+        {
+          return 1 - (value * value);
+        }
+
+        public void BackPropOutput(float[] expected)
+        {
+          //Error dervative of the cost function
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            error[i] = outputs[i] - expected[i];
+            averageError += (float)Math.Pow(outputs[i] - expected[i], 2);
+          }
+          averageError = averageError / (float)numberOfOuputs;
+
+          //Gamma calculation
+          for (int i = 0; i < numberOfOuputs; i++)
+            gamma[i] = error[i] * TanHDer(outputs[i]);
+
+          //Caluclating detla weights
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            for (int j = 0; j < numberOfInputs; j++)
+            {
+              weightsDelta[i, j] = gamma[i] * inputs[j];
+            }
+          }
+        }
+
+        public void BackPropHidden(float[] gammaForward, float[,] weightsFoward)
+        {
+          //Caluclate new gamma using gamma sums of the forward layer
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            gamma[i] = 0;
+
+            for (int j = 0; j < gammaForward.Length; j++)
+            {
+              gamma[i] += gammaForward[j] * weightsFoward[j, i];
+            }
+
+            gamma[i] *= TanHDer(outputs[i]);
+          }
+
+          //Caluclating detla weights
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            for (int j = 0; j < numberOfInputs; j++)
+            {
+              weightsDelta[i, j] = gamma[i] * inputs[j];
+            }
+          }
+        }
+        public void UpdateWeights()
+        {
+          for (int i = 0; i < numberOfOuputs; i++)
+          {
+            for (int j = 0; j < numberOfInputs; j++)
+            {
+              weights[i, j] -= weightsDelta[i, j] * 0.1f;
+            }
+          }
+        }
+      }
+    }
+
+    bool imageSetuped = false;
+    OpenFileDialog file = new OpenFileDialog();
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+      {
+        var bitmap = new Bitmap(file.FileName);
+        if (bitmap.Width > 1920 && bitmap.Height > 1080)
+        {
+          MessageBox.Show("Sorry, image is too big");
+          imageSetuped = false;
+        }
+        else
+        {
+          pictureBox1.Image = bitmap;
+          imageSetuped = true;
+        }
+      }
+    }
+
+    int whichLibrary = 0;
+    bool libraryChoosed = false;
+    private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      libraryChoosed = false;
+      for (int i = 0; i < checkedListBox1.Items.Count; i++)
+      {
+        if (checkedListBox1.GetItemCheckState(i).ToString() == "Checked")
+        {
+          libraryChoosed = true;
+          whichLibrary = i + 1;
+        }
+      }
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      if (imageSetuped)
+      {
+        if (whichLibrary <= 3 && whichLibrary >= 1 && libraryChoosed)
+        {
+          if (whichLibrary == 1)
+          {
+            try
+            {
+              var Ocr = new IronTesseract();
+              using (var Input = new OcrInput(file.FileName))
+              {
+                var Result = Ocr.Read(Input);
+                textBox1.Text = Result.Text;
+              }
+            }
+            catch
+            {
+              MessageBox.Show("Sorry, something went wrong");
+            }
+          }
+          if (whichLibrary == 2)
+          {
+            try
+            {
+              AspriseOCR.SetUp();
+              AspriseOCR ocr = new AspriseOCR();
+              ocr.StartEngine("eng", AspriseOCR.SPEED_FASTEST);
+              string path = file.FileName;
+              string Result = ocr.Recognize(path, -1, -1, -1, -1, -1, AspriseOCR.RECOGNIZE_TYPE_ALL, AspriseOCR.OUTPUT_FORMAT_PLAINTEXT);
+              textBox1.Text = Result;
+              ocr.StopEngine();
+            }
+            catch
+            {
+              MessageBox.Show("Sorry, something went wrong");
+            }
+          }
+          if (whichLibrary == 3)
+          {
+            try
+            {
+              using (Vintasoft.Imaging.ImageCollection images = new Vintasoft.Imaging.ImageCollection())
+              {
+                images.Add(file.FileName);
+                using (Vintasoft.Imaging.Ocr.Tesseract.TesseractOcr tesseractOcr = new Vintasoft.Imaging.Ocr.Tesseract.TesseractOcr())
+                {
+                  tesseractOcr.Init(new Vintasoft.Imaging.Ocr.OcrEngineSettings(Vintasoft.Imaging.Ocr.OcrLanguage.English));
+                  foreach (Vintasoft.Imaging.VintasoftImage image in images)
+                  {
+                    Vintasoft.Imaging.Ocr.Results.OcrPage ocrResult = tesseractOcr.Recognize(image);
+                    textBox1.Text = ocrResult.GetText();
+                  }
+                }
+              }
+            }
+            catch
+            {
+              MessageBox.Show("Sorry, something went wrong");
+            }
+          }
+        }
+        else
+        {
+          NeuralNetwork net = new NeuralNetwork(new int[] { 1024, 500, 10 });
+          var bitmap = new Bitmap(file.FileName);
+          var NewBitmap = ScaleImage(bitmap, 32, 32);
+          ConverPicture(NewBitmap);
+          float[] results = new float[2];
+          var data = new StreamReader("Picture.txt");
+          int sum = 1;
+          float[][] OnesAndZerosInUserTest = new float[sum][];
+          // Convert to array of boll-type symbols
+          for (int i = 0; i < sum; i++)
+          {
+            OnesAndZerosInUserTest[i] = new float[1024];
+            for (int j = 0; j < 1024; j++)
+            {
+              var character = data.Read();
+              if (character != 32)
+                OnesAndZerosInUserTest[i][j] = character - '0';
+              else
+                j--;
+            }
+          }
+          data.Close();
+          //Work of neural network
+          for (int i = 0; i < sum; i++)
+          {
+            results = net.FeedForward(OnesAndZerosInUserTest[i]);
+            float maxOutPut = results[0];
+            int index = 0;
+            for (int j = 0; j < 10; j++)
+            {
+              if (results[j] > maxOutPut)
+              {
+                index = j;
+                maxOutPut = results[j];
+              }
+
+            }
+            textBox1.Text = index.ToString();
+          }
+        }
+      }
+      else
+      {
+        MessageBox.Show("Image is not seted");
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      SaveFileDialog saveFileDialog = new SaveFileDialog();
+      saveFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+      if (saveFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+        streamWriter.WriteLine(textBox1.Text);
+        streamWriter.Close();
+      }
+    }
+  }
+}
